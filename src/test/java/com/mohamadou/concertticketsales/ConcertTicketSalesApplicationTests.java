6f7a5274235b575b745c91af4b0efd7c58dde1b8
mohamadou <mohamadou00@gmail.com>
2022-08-09 02:17:47 +0000

    Initial commit

---
+++ src/test/java/com/mohamadou/concertticketsales/ConcertTicketSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamadou.concertticketsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
